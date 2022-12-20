544022391ce2bc55933b45e2a5cf1afbb082d2c3
MuhammadRauf276 <mrauf6826604@gmail.com>
2022-12-20 18:52:57 +0500

    Week 2 lab

---
+++ task009.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main(){
+system("color 07");
+cout << "   -------------------------------- " <<endl;
+cout << "      O     ^__^            " <<endl;
+cout << "       O    (OO)| ______  /" <<endl;
+cout << "            (  )/       )_/ " <<endl;
+cout << "                ||____W| " <<endl;
+cout << "                ||    || " <<endl;
+}
+
---
+++ task05.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main(){
+cout << "*************************************************"<<endl;
+cout <<" *                                               *"<<endl;
+cout <<" *                                               *"<<endl;
+cout <<" *                                               *"<<endl;
+cout <<" *         University Admession                  *"<<endl;
+cout <<" *          Mangment   System                    * "<<endl;
+cout <<" *                                               *"<<endl;
+cout <<" *                                               *"<<endl;
+cout <<" *                                               *"<<endl;
+cout <<" *************************************************"<<endl;
+}
